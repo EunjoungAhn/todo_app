def8fbce45fda327a207859a32d3a485fc31265b
EunjoungAhn <aej9300@gmail.com>
2022-12-20 22:02:08 +0900

    폰트 관련 설정 및 폰트 적용 > 적용 확인

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.amber,
+        fontFamily: 'GmarketSansTTF',
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
