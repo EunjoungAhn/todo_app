dfa5f58e0d7cc57cfb20542b7ad7d2253f8ad1b4
EunjoungAhn <aej9300@gmail.com>
2022-12-27 21:05:32 +0900

    글자 크기, 배경 색상, 카드박스 사이즈 추가

--- lib/components/app_constants.dart
+++ lib/components/app_constants.dart
@@ -22,3 +22,18 @@ EdgeInsets.symmetric(horizontal: 6);
 /// 하단의 버튼 패딩
 const EdgeInsetsGeometry submitButtonBoxPadding =
 EdgeInsets.symmetric( horizontal: 20, vertical: 10,);
+
+/// 컬러
+Color mainColor = Color(0xFF81d0d6);
+Color bgColor = Colors.white;
+Color ibgColor = Colors.grey.withOpacity(0.1);
+Color txtColor = Colors.black;
+Color iTxtColor = Colors.black38;
+
+/// 카드 박스 사이즈
+double cardSize = 150;
+
+/// 글자 크기
+TextStyle smallTS = TextStyle(fontSize: 12);
+TextStyle middleTS = TextStyle(fontSize: 16);
+TextStyle largeTS = TextStyle(fontSize: 20);
